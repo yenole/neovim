14f07c9a747b0efb9e95045d2d17b7c9846f3461
yenole <xusir92@gmail.com>
2023-01-10 23:05:52 +0800

    feat: update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 vim.keymap.set("n", "<leader>w", "<C-W>")
-vim.keymap.set("n", "<leader>s", "<cmd>w<CR>")
 vim.keymap.set("t", "<m-q>", "<C-\\><C-n>")
 
 vim.keymap.set("i", "jk", "<ESC>")
 
+--file
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "saving file" })
+
+-- delete lazyvim keymap
 vim.keymap.del("n", "<leader>gG")

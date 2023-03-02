99254b058aa97378d7237ba7413a58b7c8064a99
yenole <xusir92@gmail.com>
2023-03-02 12:07:25 +0800

    feat: update treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,10 +4,8 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = function(_, opts)
-			opts.ensure_installed = { "help", "html", "javascript", "json", "lua", "query", "tsx", "typescript", "vim" }
-			if vim.fn.has("win32") == 1 then
-				-- opts.ensure_installed = {}
-			end
+			opts.ensure_installed =
+				{ "help", "html", "javascript", "json", "lua", "query", "tsx", "typescript", "vim", "http" }
 		end,
 	},
 }

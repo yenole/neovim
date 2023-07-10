3edf1ebdc985cbf8adc8f499cf13da091c16e646
yenole <xusir92@gmail.com>
2023-07-10 23:51:52 +0800

    feat: change lsprestart

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -3,7 +3,7 @@ return {
 		"neovim/nvim-lspconfig",
 		init = function()
 			local keys = require("lazyvim.plugins.lsp.keymaps").get()
-			table.insert(keys, { "<leader>cL", "<cmd>LspRestart<cr>", desc = "Lsp restart" })
+			table.insert(keys, { "<leader>cL", "<cmd>LspRestart<cr>", desc = "Lsp Restart" })
 		end,
 	},
 }

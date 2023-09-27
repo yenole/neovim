0adeef02cecec4c90e8f5655636c7996ed4022eb
Super User <root@Yoruno>
2023-09-27 12:12:21 +0800

    feat: add codeium.nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -6,4 +6,30 @@ return {
 			table.insert(keys, { "<leader>cL", "<cmd>LspRestart<cr>", desc = "Lsp Restart" })
 		end,
 	},
+
+	{
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		opts = {},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"Exafunction/codeium.nvim",
+		},
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			opts.sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "codeium" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			})
+			return opts
+		end,
+	},
 }

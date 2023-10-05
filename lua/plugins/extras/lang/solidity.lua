6dd6658200cb95346df109a9b733560d782f09cd
Super User <root@Yoruno>
2023-10-05 21:49:02 +0800

    fix: change null-ls.nvim

--- lua/plugins/extras/lang/solidity.lua
+++ lua/plugins/extras/lang/solidity.lua
@@ -12,7 +12,7 @@ return {
     {
         "nvimtools/none-ls.nvim",
         opts = function(_, opts)
-            local nls = require("none-ls")
+            local nls = require("null-ls")
             table.insert(opts.sources, nls.builtins.formatting.prettier.with({ extra_filetypes = { "solidity" } }))
         end,
     },

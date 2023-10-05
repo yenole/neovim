23278cb8983fe71974bdf0de8e1afe782d55036f
Super User <root@Yoruno>
2023-10-05 14:52:39 +0800

    feat: change null-ls.nvim

--- lua/plugins/extras/lang/golang.lua
+++ lua/plugins/extras/lang/golang.lua
@@ -1,47 +1,48 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "go" })
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "goimports", "gopls", "delve" })
-		end,
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, { "go" })
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, { "goimports", "gopls", "delve" })
+        end,
+    },
 
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		opts = function(_, opts)
-			table.insert(opts.sources, require("null-ls").builtins.formatting.goimports)
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				gopls = {
-					keys = {
-						{
-							"<leader>dr",
-							function()
-								require("dap").continue()
-							end,
-							desc = "Run (Go)",
-						},
-					},
-				},
-			},
-		},
-	},
-	{
-		"mfussenegger/nvim-dap",
-		opts = function(_, _)
-			local dap = require("dap")
-			dap.adapters.go = dap.adapters.delve
-			require("dap.ext.vscode").load_launchjs()
-		end,
-	},
+    {
+        "nvimtools/none-ls.nvim",
+        opts = function(_, opts)
+            vim.notify()
+            table.insert(opts.sources, require("null-ls.nvim").builtins.formatting.goimports)
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                gopls = {
+                    keys = {
+                        {
+                            "<leader>dr",
+                            function()
+                                require("dap").continue()
+                            end,
+                            desc = "Run (Go)",
+                        },
+                    },
+                },
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
+        opts = function(_, _)
+            local dap = require("dap")
+            dap.adapters.go = dap.adapters.delve
+            require("dap.ext.vscode").load_launchjs()
+        end,
+    },
 }
--- lua/plugins/extras/lang/solidity.lua
+++ lua/plugins/extras/lang/solidity.lua
@@ -1,43 +1,43 @@
 return {
 
-	-- add solidity to treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = function(_, opts)
-			table.insert(opts.ensure_installed, "solidity")
-		end,
-	},
+    -- add solidity to treesitter
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            table.insert(opts.ensure_installed, "solidity")
+        end,
+    },
 
-	-- add solidity to null-ls
-	{
-		"jose-elias-alvarez/null-ls.nvim",
-		opts = function(_, opts)
-			local nls = require("null-ls")
-			table.insert(opts.sources, nls.builtins.formatting.prettier.with({ extra_filetypes = { "solidity" } }))
-		end,
-	},
-	-- add solidity to lspconfig
-	-- {
-	-- 	"neovim/nvim-lspconfig",
-	-- 	opts = function(_, opts)
-	-- 		opts.servers.solidity = {
-	-- 			cmd = { "solidity-ls", "--stdio" },
-	-- 			filetypes = { "solidity" },
-	-- 			root_dir = require("lazyvim.util").get_root,
-	-- 		}
-	-- 	end,
-	--   {
-	-- 	solidity = {
-	-- 		cmd = { "solidity-language-server", "--stdio" },
-	-- 		filetypes = { "solidity" },
-	-- 		root_dir = require("lspconfig.util").root_pattern(".git", "package.json"),
-	-- 	},
-	-- },
-	-- function()
-	-- 	local registry = require("mason-registry")
-	-- 	local pkg = registry.get_package("solidity-ls")
-	-- 	local path = pkg:get_install_path()
-	-- 	print(vim.inspect(opts))
-	-- end,
-	-- },
+    -- add solidity to null-ls
+    {
+        "nvimtools/none-ls.nvim",
+        opts = function(_, opts)
+            local nls = require("none-ls")
+            table.insert(opts.sources, nls.builtins.formatting.prettier.with({ extra_filetypes = { "solidity" } }))
+        end,
+    },
+    -- add solidity to lspconfig
+    -- {
+    -- 	"neovim/nvim-lspconfig",
+    -- 	opts = function(_, opts)
+    -- 		opts.servers.solidity = {
+    -- 			cmd = { "solidity-ls", "--stdio" },
+    -- 			filetypes = { "solidity" },
+    -- 			root_dir = require("lazyvim.util").get_root,
+    -- 		}
+    -- 	end,
+    --   {
+    -- 	solidity = {
+    -- 		cmd = { "solidity-language-server", "--stdio" },
+    -- 		filetypes = { "solidity" },
+    -- 		root_dir = require("lspconfig.util").root_pattern(".git", "package.json"),
+    -- 	},
+    -- },
+    -- function()
+    -- 	local registry = require("mason-registry")
+    -- 	local pkg = registry.get_package("solidity-ls")
+    -- 	local path = pkg:get_install_path()
+    -- 	print(vim.inspect(opts))
+    -- end,
+    -- },
 }

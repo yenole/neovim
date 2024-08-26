c84e8b585f85dd860225af8f344156447e8dfe2b
yoruno <xusir92@gmail.com>
2024-08-26 18:33:02 +0800

    feat: 替换右侧函数导航

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -16,4 +16,18 @@ return {
 			table.insert(opts.sources, 1, { name = "codeium", group_index = 1 })
 		end,
 	},
+
+	-- outline
+	{
+		"hedyhli/outline.nvim",
+		keys = { { "<leader>co", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
+		opts = {
+			outline_window = {
+				width = 20,
+				relative_width = true,
+				auto_close = true,
+				auto_jump = true,
+			},
+		},
+	},
 }

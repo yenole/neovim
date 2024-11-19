9d2dcea9bb70b4ea00c03e858648f3768d416336
Yoruno <xusir92@gmail.com>
2024-11-19 20:32:49 +0800

    style: 去除注释

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,11 +1,9 @@
 return {
-	--
 	--language
-	-- { import = "plugins.extras.lang.php" },
+	{ import = "plugins.extras.lang.php" },
 	{ import = "plugins.extras.lang.ts" },
 	{ import = "plugins.extras.lang.go" },
 	{ import = "plugins.extras.lang.rust" },
-	-- { import = "plugins.extras.lang.kotlin" },
 
 	--tools
 	{ import = "plugins.extras.http" },

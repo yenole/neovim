8709cc0b9de77fefed0c5bcf209b730e394b3197
yenole <xusir92@gmail.com>
2026-03-29 22:26:25 +0800

    feat: 新增LazySQL

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -7,6 +7,13 @@ return {
 			{ "<leader>tn", require("utils.terminal").terminal, desc = "New Terminal" },
 			{ "<leader>to", require("utils.terminal").output, desc = "New Terminal" },
 			{ "<leader>tl", require("utils.terminal").term_select, desc = "Terminals" },
+			{
+				"<leader>ts",
+				function()
+					require("utils.terminal").terminal("lazysql", { name = "LazySQL" })
+				end,
+				desc = "Toggle LazySQL",
+			},
 		},
 		opts = { direction = "float", float_opts = { border = "curved", title_pos = "center" } },
 	},
